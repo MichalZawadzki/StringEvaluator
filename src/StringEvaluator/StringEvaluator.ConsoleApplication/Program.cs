ce0e2def34bfcacede7b0870b614609fe67d0d43
Michal Zawadzki <zawadzkimichal7@gmail.com>
2019-10-04 23:55:44 +0200

    Add evaluation execution in console application

--- src/StringEvaluator/StringEvaluator.ConsoleApplication/Program.cs
+++ src/StringEvaluator/StringEvaluator.ConsoleApplication/Program.cs
@@ -6,7 +6,33 @@ namespace StringEvaluator.ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("******");
+            Console.WriteLine("Please put expression consisting of non-negative integers and the [+ - / *] operators only, then press enter:");
+            Console.WriteLine("******");
+
+            Console.WriteLine();
+            Console.Write("Expression: ");
+            string expression = Console.ReadLine();
+
+            try
+            {
+                BasicEvaluator basicEvaluator = new BasicEvaluator();
+                decimal result = basicEvaluator.Evaluate(expression);
+
+                Console.WriteLine();
+                Console.WriteLine($"Result: {result}");
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"Error: {ex.Message}");
+                Console.ResetColor();
+            }
+
+            Console.WriteLine();
+            Console.Write("Press any key...");
+            Console.ReadKey();
         }
     }
 }
